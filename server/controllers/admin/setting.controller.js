d75d095cff2185d9d09a727729da85d285973264
DORMIVEGLIA1001 <mha17003@gmail.com>
2025-09-06 08:23:19 +0700

    minh-permission

--- server/controllers/admin/setting.controller.js
+++ server/controllers/admin/setting.controller.js
@@ -1,35 +1,83 @@
+const SettingWebsiteInfo = require("../../models/setting-website-info.model");
+const Role = require("../../models/role.model");
+const AccountAdmin = require("../../models/account-admin.model");
+
+const permissionConfig = require("../../config/permission");
+const bcrypt = require("bcryptjs");
+const { model } = require("mongoose");
+
 module.exports.list = async (req, res) => {
-  res.render("admin/pages/setting-list.pug", {
+  res.render("admin/pages/setting-list", {
     pageTitle: "Cài đặt chung"
   })
 };
 
 module.exports.websiteInfo = async (req, res) => {
+
+  const settingWebsiteInfo = await SettingWebsiteInfo.findOne({});
+
   res.render("admin/pages/setting-website-info", {
-    pageTitle: "Thông tin website"
+    pageTitle: "Thông tin website",
+    settingWebsiteInfo: settingWebsiteInfo,
   })
 }
 
-module.exports.accountAdminList = async (req, res) => {
-  res.render("admin/pages/setting-account-admin-list", {
-    pageTitle: "Tài khoản quản trị"
+module.exports.websiteInfoPatch = async (req, res) => {
+  if(req.files && req.files.logo){
+    req.body.logo = req.files.logo[0].path; 
+  }
+  else{
+    delete req.body.logo;
+  }
+
+  const settingWebsiteInfo = await SettingWebsiteInfo.findOne({});
+
+  if(settingWebsiteInfo){
+    await SettingWebsiteInfo.updateOne({
+      _id: settingWebsiteInfo.id
+    }, req.body);
+  }
+  else{
+    const newRecord = new SettingWebsiteInfo(req.body);
+    await newRecord.save();
+  }
+
+  req.flash("success", "Cập nhật thành công!");
+
+  res.json({
+    code: "success"
   })
 }
 
-module.exports.accountAdminCreate = async (req, res) => {
-  res.render("admin/pages/setting-account-admin-create", {
-    pageTitle: "Tạo tài khoản quản trị"
-  })
+module.exports.accountAdmin = async (req, res) => {
+  const accountAdminList = await AccountAdmin
+   .find({
+    deleted: false
+   })
+   .sort({ 
+    createdAt: "desc" 
+  });
+  
+  for(const item of accountAdminList) {
+    if(item.role){
+      const roleIfo = await Role.findOne({
+        _id: item.role
+      });
+
+      if(roleIfo) {
+        item.roleName = roleIfo.roleName;
+      }
+    }
+
+    }
+
+    res.render("admin/pages/setting-account-admin-list", {
+      pageTitle: "Quản trị viên",
+      accountAdminList: accountAdminList,
+    })
 }
 
-module.exports.roleList = async (req, res) => {
-  res.render("admin/pages/setting-role-list", {
-    pageTitle: "Nhóm quyền"
-  })
-}
 
-module.exports.roleCreate = async (req, res) => {
-  res.render("admin/pages/setting-role-create", {
-    pageTitle: "Tạo nhóm quyền"
-  })
-}
+
+
+
