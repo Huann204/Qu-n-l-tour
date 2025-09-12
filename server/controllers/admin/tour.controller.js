57f2b9cfc8f6c83114a11e921e7f7dbe66d87e40
Cuong Vu <156158430+CuongVu1920@users.noreply.github.com>
2025-09-12 11:09:45 +0700

    Tích hợp giao diện quản lý tour

---
+++ server/controllers/admin/tour.controller.js
@@ -0,0 +1,6 @@
+module.exports.list = async (req, res) => {
+res.render("admin/pages/tour-list", {
+    pageTitle: "Quản lý tour",
+  })
+}
+
--- server/routers/admin/index.route.js
+++ server/routers/admin/index.route.js
@@ -2,11 +2,15 @@ const router = require("express").Router();
 const accountRoutes = require("./account.route");
 const categoryRoutes = require("./category.route");
 const settingRoutes = require("./setting.route");
+const tourRoutes = require("./tour.route");
+
 
 const authMiddleware = require("../../middlewares/admin/auth.middleware");
 
 router.use('/account', accountRoutes);
 router.use('/category',authMiddleware.verifyToken, categoryRoutes);
-router.use('/setting',authMiddleware.verifyToken, settingRoutes)
+router.use('/setting',authMiddleware.verifyToken, settingRoutes);
+router.use('/tour',authMiddleware.verifyToken, tourRoutes);
+
 
 module.exports = router;
