e2de12929c387a4b09754e1d463fccbfeccf5245
Cuong Vu <156158430+CuongVu1920@users.noreply.github.com>
2025-09-17 14:37:54 +0700

    Hiển thị breadcrumb (danh sách tour)

--- server/controllers/client/tour.controller.js
+++ server/controllers/client/tour.controller.js
@@ -1,14 +1,3 @@
-const Tour = require("../../models/tour.model");
-
-module.exports.list = async (req, res) => {
-  const tourList = await Tour.find({});
-
-  res.render("client/pages/tour-list", {
-    pageTitle: "Danh sách tour",
-    tourList: tourList
-  })
-}
-
 module.exports.detail = async (req, res) => {
   res.render("client/pages/tour-detail.pug", {
     pageTitle: "Chi tiết tour"
