7a671f7066bfec88cef68e7eaf465b3d26d0ae0a
Cuong Vu <156158430+CuongVu1920@users.noreply.github.com>
2025-09-18 22:26:30 +0700

    hiển thị dữ liệu tin tức ra trang client

--- server/controllers/client/category-blog.controller.js
+++ server/controllers/client/category-blog.controller.js
@@ -1,5 +1,38 @@
+const Blog = require("../../models/blog.model");
+const CategoryBlog = require("../../models/category-blog.model")
+
+
 module.exports.list = async (req, res) => {
+
+  const categoryBlogList = await CategoryBlog
+    .find({
+      deleted: false
+    })
+    .sort({
+      position: "asc"
+    });
+
+    for (const item of categoryBlogList) {
+        const totalBlog = await Blog.countDocuments({
+          parent: item.id,
+          deleted: false
+        })
+
+        item.totalBlog = totalBlog;
+    }
+
+  const newBlogList = await Blog.find({
+    deleted: false,
+    status: "active"
+  })
+  .sort({
+    createdAt: "asc"
+  })
+  .limit(4);
+
   res.render("client/pages/category-blog.pug", {
     pageTitle: "Liên hệ",
+    categoryBlogList: categoryBlogList,
+    newBlogList: newBlogList
   })
 }
