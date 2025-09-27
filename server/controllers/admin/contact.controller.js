3d83c483cd2ce0a0f857103d587f45d06726baa7
Cuong Vu <156158430+CuongVu1920@users.noreply.github.com>
2025-09-27 08:12:13 +0700

    Bổ sung chức năng trang thông tin liên hệ

--- server/controllers/admin/contact.controller.js
+++ server/controllers/admin/contact.controller.js
@@ -7,6 +7,56 @@ module.exports.list = async (req, res) => {
     deleted: false
   };
 
+   const dateFilter = {};
+  
+    if(req.query.startDate) {    
+      const startDate = moment(req.query.startDate).startOf("date").toDate();
+      dateFilter.$gte = startDate;    
+    };
+  
+     if(req.query.endDate) {
+      const endDate = moment(req.query.endDate).endOf("date").toDate();
+      dateFilter.$lte = endDate;    
+    };
+    // gte = greater than or equal
+    // lte = less than or equal
+    
+    if(Object.keys(dateFilter).length > 0) {
+      find.createdAt = dateFilter;
+    };
+
+  // Pagination
+  const limitItems = 5;
+  let page = 1;
+
+  if(req.query.page) {
+    const currentPage = parseInt(req.query.page);
+
+    if(currentPage > 0) {
+      page = currentPage;
+    };
+  };
+  
+  const totalRecord = await Contact.countDocuments(find);
+  const totalPage = Math.ceil(totalRecord / limitItems);
+
+  if(page > totalPage) {
+    page = totalPage
+  };
+
+  if(totalPage === 0) {
+    page = 1;
+  };
+  
+  const skip = (page - 1) * limitItems;
+  const pagination = {
+    skip: skip,
+    totalRecord: totalRecord,
+    totalPage: totalPage,
+  }
+
+  // End Pagination
+
   const contactList = await Contact
     .find(find)
     .sort({
@@ -20,6 +70,60 @@ module.exports.list = async (req, res) => {
 
   res.render("admin/pages/contact-list", {
     pageTitle: "Thông tin liên hệ",
-    contactList: contactList
+    contactList: contactList,
+    pagination: pagination
   })
+}
+
+module.exports.deletePatch = async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    await Contact.updateOne({
+      _id: id
+    }, {
+      deleted: true,
+      deletedBy: req.account.id,
+      deletedAt: Date.now()
+    });
+    
+
+    req.flash("success", "Xóa liên hệ thành công!");
+
+    res.json({
+      code: "success",
+    })
+  } catch (error) {
+    res.json({
+      code: "error",
+      message: "Id không hợp lệ!"
+    });
+  };
+}
+
+module.exports.changeMultiPatch = async (req, res) => {
+  try {
+    const { option, ids } = req.body;
+
+    switch (option) {
+      case "delete":
+        await Contact.updateMany({
+          _id: { $in: ids }
+        }, {
+          deleted: true,
+          deletedAt: Date.now(),
+          deletedBy: req.account.id,
+        });
+        req.flash("success", "Xóa thành công!");
+        break;
+    }
+    res.json({
+      code: "success",
+    });
+  } catch (error) {
+    res.json({
+      code: "error",
+      message: "Id tồn tại trong hệ thống!"
+    })
+  }
 }
